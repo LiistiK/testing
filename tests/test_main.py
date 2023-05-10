1b40a2efd6be9129eb77c8ca8c9909861ffedb1a
Гамид <95250214+LiistiK@users.noreply.github.com>
2023-05-10 19:59:39 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,9 +20,6 @@ class TestFunctions(unittest.TestCase):
     def tearDown(self) -> None:
         print('tearDown --> work')
 
-    def setUp(self) -> None:
-        print('setUp --> work')
-
     @patch('builtins.input', return_value="2207 876234")
     def test_using_side_effect1(self, mock_input):
         result = "Василий Гупкин"
@@ -37,37 +34,16 @@ class TestFunctions(unittest.TestCase):
         user_list = {'Василий Гупкин', 'Геннадий Покемонов', 'Михаил'}
         self.assertEqual(main.get_all_doc_owners_names(), user_list)
 
-    def tearDown(self) -> None:
-        print('tearDown --> work')
-
-    def setUp(self) -> None:
-        print('setUp --> work')
-
     @patch('builtins.input', return_value='1')
     def test_add_new_shelf(self, mock_input):
         shelt = ['2207 876234', '11-2', '5455 028765']
         self.assertEqual(main.add_new_shelf(), (('1'), False))
 
-    def tearDown(self) -> None:
-        print('tearDown --> work')
-
-    def setUp(self) -> None:
-        print('setUp --> work')
-
     def test_add_new_doc(self):
         self.assertEqual(main.add_new_doc('7311', 'pass', 'Михаил', 2), 2)
 
-    def tearDown(self) -> None:
-        print('tearDown --> work')
-
-    def setUp(self) -> None:
-        print('setUp --> work')
-
     def test_delete_doc(self):
         self.assertTrue(main.delete_doc('10006'))
-
-    def tearDown(self) -> None:
-        print('tearDown --> work')
 
 if __name__ == '__main__':
     unittest.main()
